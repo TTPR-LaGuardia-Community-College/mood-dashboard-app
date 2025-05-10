2a72dc390f5212c69066eb3ebb9ed8be74c74236
Noah Alayon <noahalayon01@gmail.com>
2025-05-10 16:55:10 -0400

    Added mood percentage stats

--- src/App.jsx
+++ src/App.jsx
@@ -44,4 +44,5 @@ function App() {
     </div>
   );
 }
+
 export default App
--- src/components/MoodStats.jsx
+++ src/components/MoodStats.jsx
@@ -4,14 +4,26 @@ function MoodStats({ counts=0 }) {
      - Determine percentages
      - Render a simple table or list
   */
+  let total = 0;
+  Object.values(counts).forEach((value) => total += value);
+  
+  let happy = 0, tired = 0, excited = 0, meh = 0;
+
+  if(total !== 0) {
+    happy = ((counts["happy"]/total) * 100).toFixed(2);
+    tired = ((counts["tired"]/total) * 100).toFixed(2);
+    excited = ((counts["excited"]/total) * 100).toFixed(2);
+    meh = ((counts["meh"]/total) * 100).toFixed(2);
+  }
+
   return (
     <section className="stats">
-      <h2>Total votes: _</h2>
+      <h2>Total votes: {total}</h2>
       <ul>
-        <li>😊 Happy: _%</li>
-        <li>😴 Tired: _%</li>
-        <li>🤩 Excited: _%</li>
-        <li>😐 Meh: _%</li>
+        <li>😊 Happy: {happy}%</li>
+        <li>😴 Tired: {tired}%</li>
+        <li>🤩 Excited: {excited}%</li>
+        <li>😐 Meh: {meh}%</li>
       </ul>
     </section>
   );
