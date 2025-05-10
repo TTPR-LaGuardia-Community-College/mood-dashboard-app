d06ee6a6fc8c30098ca6f53183184aecda3e032a
Carlos A. Soler <CARLOSSOLERG319@GMAIL.COM>
2025-05-10 15:25:05 -0400

    Homework emoji counter

--- src/components/MoodStats.jsx
+++ src/components/MoodStats.jsx
@@ -4,15 +4,23 @@ function MoodStats({ counts=0 }) {
      - Determine percentages
      - Render a simple table or list
   */
+  const totalVotes = counts.happy + counts.tired + counts.excited + counts.meh;
+
+     
   return (
     <section className="stats">
-      <h2>Total votes: _</h2>
+      <h2>Total votes: {(totalVotes)}</h2>
       <ul>
-        <li>😊 Happy: _%</li>
-        <li>😴 Tired: _%</li>
-        <li>🤩 Excited: _%</li>
-        <li>😐 Meh: _%</li>
-      </ul>
+        <li>😊 Happy: {((counts.happy/totalVotes)*100).toPrecision(2)}%</li>
+        <li>😴 Tired: {((counts.tired/totalVotes)*100).toPrecision(2)}%</li>
+        <li>🤩 Excited: {((counts.excited/totalVotes)*100).toPrecision(2)}%</li>
+        <li>😐 Meh: {((counts.meh/totalVotes)*100).toPrecision(2)}%</li>
+      </ul>  
+    
+
+
+      
+
     </section>
   );
 }
