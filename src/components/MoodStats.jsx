a1eb4556b378e2af3c75ccd012046634547bc72f
TCHOKDUP <tenchokdup1@gmail.com>
2025-05-16 09:29:18 -0400

    push

--- src/components/MoodStats.jsx
+++ src/components/MoodStats.jsx
@@ -4,14 +4,23 @@ function MoodStats({ counts=0 }) {
      - Determine percentages
      - Render a simple table or list
   */
+
+        const totalVotes = Object.values(counts).reduce((a, b) => a + b, 0);        
+        const happyPercentage = ((counts.happy / totalVotes) * 100);
+        const tiredPercentage = ((counts.tired / totalVotes) * 100);
+        const excitedPercentage = ((counts.excited / totalVotes) * 100);
+        const mehPercentage = ((counts.meh / totalVotes) * 100);
+
+
+
   return (
     <section className="stats">
-      <h2>Total votes: _</h2>
+      <h2>Total votes: {totalVotes}</h2>
       <ul>
-        <li>😊 Happy: _%</li>
-        <li>😴 Tired: _%</li>
-        <li>🤩 Excited: _%</li>
-        <li>😐 Meh: _%</li>
+        <li>😊 Happy: {happyPercentage}_%</li>
+        <li>😴 Tired: {tiredPercentage}_%</li>
+        <li>🤩 Excited:{excitedPercentage} _%</li>
+        <li>😐 Meh: {mehPercentage}_%</li>
       </ul>
     </section>
   );
