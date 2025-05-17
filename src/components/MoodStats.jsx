6a22972de5597ef3f7bb3938bbf6c5c485c07506
Daniel Na <DNA@DESKTOP-GKC3M84>
2025-05-17 13:21:21 -0400

    Updated MoodStats.jsx to display vote counts & added reflection to README

--- src/components/MoodStats.jsx
+++ src/components/MoodStats.jsx
@@ -1,17 +1,28 @@
-function MoodStats({ counts=0 }) {
+
+function MoodStats({ counts }) {
   /* TODO #5
      - Calculate total votes
      - Determine percentages
      - Render a simple table or list
   */
+ function total(counts, vote) {
+  const totalVotes = counts.happy + counts.tired + counts.excited + counts.meh;
+  if(totalVotes == 0) {
+    return 0;
+  }
+  else {
+    return ((vote / totalVotes)*100).toFixed(2);
+  }
+ }
+
   return (
     <section className="stats">
-      <h2>Total votes: _</h2>
+      <h2>Total votes: {counts.happy + counts.tired + counts.excited + counts.meh}</h2>
       <ul>
-        <li>😊 Happy: _%</li>
-        <li>😴 Tired: _%</li>
-        <li>🤩 Excited: _%</li>
-        <li>😐 Meh: _%</li>
+        <li>😊 Happy: {counts.happy} {total(counts, counts.happy)}%</li>
+        <li>😴 Tired: {counts.tired} {total(counts, counts.tired)}%</li>
+        <li>🤩 Excited: {counts.excited} {total(counts, counts.excited)}%</li>
+        <li>😐 Meh: {counts.meh} {total(counts, counts.meh)}%</li>
       </ul>
     </section>
   );
